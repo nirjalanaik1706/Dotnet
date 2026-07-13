3e5ce8cfa864d8ee2e8b6b70392ce74a18a81399
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-07-13 13:15:07 +0530

    Reverse a each word in a sentence

---
+++ String_Problems/ReverseWordInSentence/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReverseWordsSentence;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string sentence = "Hello dear";
+        
+        string[] word = sentence.Split(' ');
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            string revSentence = "";
+            for (int j = word[i].Length - 1; j >= 0; j--)
+            {
+                revSentence+=word[i][j];
+            }
+            word[i]=revSentence;
+        }
+        // Console.WriteLine("reverse a word in a sentence: "+revSentence);
+        Console.WriteLine(string.Join(' ',word));
+    }
+}
