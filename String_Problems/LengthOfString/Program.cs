858aacf485356406243113a81502c56ec53c8fe2
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-07-13 11:53:28 +0530

    Calculate length of string

---
+++ String_Problems/LengthOfString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StringLength;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        string str = "Nirjala";   //if we add space , it also calculate the space
+        int count = 0;
+
+        foreach (char ch in str)
+        {
+            count++;
+        }
+
+        Console.WriteLine(count);
+    }
+}
