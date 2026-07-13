c3087c7ca153daa8b71e07627678dd187da55506
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-07-13 12:11:34 +0530

    Reverse a String

---
+++ String_Problems/ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReverseString;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string str = "Nirjala";
+        string revStr = "";
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            revStr += str[i];
+        }
+        Console.WriteLine("Reverse string: " + revStr);
+
+    }
+}
